4d84ced21958313ba3281bdc9a765791f8a6c3f8
Denis Medvedev <medvedev0denis@outlook.com>
2023-02-09 14:20:51 +0300

    Create pacman.sh

---
+++ pacman.sh
@@ -0,0 +1,14 @@
+pacman -S polkit
+pacman -S sway
+pacman -S waybar
+pacman -S wofi
+pacman -S swaylock
+pacman -S mako
+pacman -S light
+pacman -S bluez bluez-utils btusb
+pacman -S pipewore pipewire-jack
+pacman -S sof-firmware
+
+pacman -S evince
+pacman -S libreoffice-fresh
+pacman -S qalculate-gtk
