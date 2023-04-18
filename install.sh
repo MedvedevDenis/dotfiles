1deaf4c0a60dc2e81ccc2b9c34ee9a5a6cd53893
Medvedev Denis <medvedev0denis@gmail.com>
2023-04-18 09:09:21 +0300

    Sway config

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ echo "XDG_SESSION_TYPE=wayland" >> /etc/environment
 git config --global protocol.file.allow always
 
 systemctl enable ly.service
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
