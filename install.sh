50bc3220dbab4f7c2a47822bf991151a328951f0
Denis Medvedev <medvedev0denis@outlook.com>
2023-04-25 20:37:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-echo "XDG_SESSION_TYPE=wayland" >> /etc/environment
-echo "XDG_SESSION_DESKTOP=sway" >> /etc/environment
-echo "XDG_CURRENT_DESKTOP=sway" >> /etc/environment
-
-echo "GTK_THEME=Catppuccin-Mocha-Standard-Lavender-Dark" >> /etc/environment
-
-echo "SDL_VIDEODRIVER=wayland" >> /etc/environment
-echo "_JAVA_AWT_WM_NONREPARENTING=1" >> /etc/environment
-echo "QT_QPA_PLATFORM=wayland" >> /etc/environment
+sudo sh -c 'echo  "XDG_SESSION_TYPE=wayland"' >> /etc/environment
+sudo sh -c 'echo  "XDG_SESSION_DESKTOP=sway"' >> /etc/environment
+sudo sh -c 'echo  "XDG_CURRENT_DESKTOP=sway"' >> /etc/environment
+sudo sh -c 'echo  "GTK_THEME=Catppuccin-Mocha-Standard-Lavender-Dark"' >> /etc/environment
+sudo sh -c 'echo  "_JAVA_AWT_WM_NONREPARENTING=1"' >> /etc/environment
+sudo sh -c 'echo  "QT_QPA_PLATFORM=wayland"' >> /etc/environment
 
 
 sudo pacman -Sy --noconfirm polkit \
@@ -21,7 +18,7 @@ sudo pacman -Sy --noconfirm polkit \
                       pipewire pipewire-jack pipewire-alsa pipewire-pulse \
                       pavucontrol helvum \
                       sof-firmware \
-                      cava \
+                     
                       libreoffice-fresh \ 
                       qalculate-gtk \
                       obsidian \
@@ -37,16 +34,24 @@ sudo pacman -Sy --noconfirm polkit \
                       ttf-fira-code \
                       ttf-firacode-nerd 
 
+cd ~
+
+rm -rf .oh-my-zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 cd /opt
-git clone https://aur.archlinux.org/yay-git.git
-chown -R ${USER}:${USER} ./yay-git
+sudo rm -rf yay-git
+
+sudo git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R ${USER}:${USER} ./yay-git
 cd yay-git
 makepkg -si
 yay -Syu
+
+cd ~
 
 git config --global protocol.file.allow always
 
@@ -56,24 +61,26 @@ yay -S ly \
        catppuccin-gtk-theme-mocha \
        autotiling \
        swaylock-effects-git \
-       musikcube
-
+       musikcube  \
+       cava
+       
 systemctl enable ly.service
 systemctl enable bluetooth
 systemctl enable docker
 
+cd ~/dotfiles
 cp .zshrc /home/${USER}
 
-cp -r alacritty /home/${USER}/.config
-cp -r gtk-3.0 /home/${USER}/.config
-cp -r gtk-4.0 /home/${USER}/.config
-cp -r lazygit /home/${USER}/.config
-cp -r mako /home/${USER}/.config
-cp -r mc /home/${USER}/.config
-cp -r neofetch /home/${USER}/.config
-cp -r rofi /home/${USER}/.config
-cp -r sway /home/${USER}/.config
-cp -r waybar /home/${USER}/.config
+cp -r alacritty /home/${USER}/.config/alacritty
+cp -r gtk-3.0 /home/${USER}/.config/
+cp -r gtk-4.0 /home/${USER}/.config/
+cp -r lazygit /home/${USER}/.config/lazygit
+cp -r mako /home/${USER}/.config/mako
+cp -r mc /home/${USER}/.config/mc
+cp -r neofetch /home/${USER}/.config/neofetch
+cp -r rofi /home/${USER}/.config/rofi
+cp -r sway /home/${USER}/.config/sway
+cp -r waybar /home/${USER}/.config/waybar
 
 
 cd ~
