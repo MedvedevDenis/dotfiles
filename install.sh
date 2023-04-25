e8268c9fbd35e415e97d0e2a98d68e71841fff3f
Medvedev Denis <medvedev0denis@gmail.com>
2023-04-25 13:25:21 +0300

    install file config

--- install.sh
+++ install.sh
@@ -1,11 +1,84 @@
-
+#!/bin/bash
 
 echo "XDG_SESSION_TYPE=wayland" >> /etc/environment
+echo "XDG_SESSION_DESKTOP=sway" >> /etc/environment
+echo "XDG_CURRENT_DESKTOP=sway" >> /etc/environment
 
-# Install ly
-#
-git config --global protocol.file.allow always
+echo "GTK_THEME=Catppuccin-Mocha-Standard-Lavender-Dark" >> /etc/environment
 
-systemctl enable ly.service
+echo "SDL_VIDEODRIVER=wayland" >> /etc/environment
+echo "_JAVA_AWT_WM_NONREPARENTING=1" >> /etc/environment
+echo "QT_QPA_PLATFORM=wayland" >> /etc/environment
+
+
+sudo pacman -Sy --noconfirm polkit \
+                      xorg-xwayland \
+                      zsh alacritty neovim \
+                      sway waybar rofi swaylock swaybg \
+                      mako\
+                      light \
+                      bluez bluez-utils blueman \
+                      pipewire pipewire-jack pipewire-alsa pipewire-pulse \
+                      pavucontrol helvum \
+                      sof-firmware \
+                      cava \
+                      libreoffice-fresh \ 
+                      qalculate-gtk \
+                      obsidian \
+                      papirus-icon-theme \
+                      dbeaver \
+                      flameshot \
+                      telegram-desktop discord \
+                      evince \
+                      eog \
+                      nautilus \
+                      lutris \
+                      docker \
+                      ttf-fira-code \
+                      ttf-firacode-nerd 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cd /opt
+git clone https://aur.archlinux.org/yay-git.git
+chown -R ${USER}:${USER} ./yay-git
+cd yay-git
+makepkg -si
+yay -Syu
+
+git config --global protocol.file.allow always
+
+yay -S ly \
+       microsoft-edge-stable-bin \
+       visual-studio-code-bin \
+       catppuccin-gtk-theme-mocha \
+       autotiling \
+       swaylock-effects-git \
+       musikcube
+
+systemctl enable ly.service
+systemctl enable bluetooth
+systemctl enable docker
+
+cp .zshrc /home/${USER}
+
+cp -r alacritty /home/${USER}/.config
+cp -r gtk-3.0 /home/${USER}/.config
+cp -r gtk-4.0 /home/${USER}/.config
+cp -r lazygit /home/${USER}/.config
+cp -r mako /home/${USER}/.config
+cp -r mc /home/${USER}/.config
+cp -r neofetch /home/${USER}/.config
+cp -r rofi /home/${USER}/.config
+cp -r sway /home/${USER}/.config
+cp -r waybar /home/${USER}/.config
+
+
+cd ~
+git clone https://github.com/catppuccin/rofi
+cd rofi/basic
+bash install.sh
+cd ~
+rm -rf rofi
--- yay.sh
+++
@@ -1,4 +0,0 @@
-yay -S ly
-
-yay -S microsoft-edge-stable-bin
-yay -S catppuccin-gtk-theme-mocha
