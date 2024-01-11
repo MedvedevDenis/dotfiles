ec9a29bb8d3e4acc1016aac6d6aecce19398ef47
Denis Medvedev <medvedev0denis@gmail.com>
2024-01-12 00:36:25 +0300

    Скрипты

--- scripts/config.bash
+++ scripts/config.bash
@@ -4,13 +4,25 @@ cd ~/dotfiles
 
 cp .zshrc /home/${USER}
 
-cp -r alacritty /home/${USER}/.config/alacritty
-cp -r lazygit /home/${USER}/.config/lazygit
-cp -r mako /home/${USER}/.config/mako
-cp -r mc /home/${USER}/.config/mc
-cp -r neofetch /home/${USER}/.config/neofetch
-cp -r sway /home/${USER}/.config/sway
-cp -r waybar /home/${USER}/.config/waybar
+
+cp -r .config/alacritty /home/${USER}/.config/alacritty
+cp -r .config/lazygit /home/${USER}/.config/lazygit
+cp -r .config/mako /home/${USER}/.config/mako
+cp -r .config/mc /home/${USER}/.config/mc
+cp -r .config/neofetch /home/${USER}/.config/neofetch
+cp -r .config/sway /home/${USER}/.config/sway
+cp -r .config/swaylock /home/${USER}/.config/swaylock
+cp -r .config/waybar /home/${USER}/.config/waybar
 
 
-git clone https://github.com/catppuccin/alacritty.git ~/.config/alacritty/catppuccin
+mkdir tmp
+cd tmp
+
+git clone https://github.com/catppuccin/alacritty &&  cp ./alacritty/catppuccin-mocha.toml $HOME/.config/alacritty
+git clone https://github.com/catppuccin/waybar && cp ./waybar/themes/mocha.css $HOME/.config/waybar
+git clone https://github.com/catppuccin/btop && cp ./btop/themes/catppuccin_mocha.theme $HOME/.config/btop/themes
+git clone https://github.com/catppuccin/i3 && cp ./i3/themes/catppuccin-mocha $HOME/.config/sway
+git clone https://github.com/catppuccin/mc && mkdir -p $HOME/.local/share/mc/skins && cp ./mc/catppuccin.ini $HOME/.local/share/mc/skins
+
+cd ..
+rm -rf tmp
---
+++ scripts/install-gaming-packages.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Включение multilib"
+
+sudo nano /etc/pacman.conf
+
+sudo pacman -Sy --noconfirm wine-staging \
+                      vulkan-icd-loader lib32-vulkan-icd-loader \
+                      @vulkan-intel lib32-vulkan-intel \
+                      nvidia-utils lib32-nvidia-utils \
+                      vulkan-radeon lib32-vulkan-radeon \
+                      lib32-pipewire lib32-alsa-lib lib32-alsa-plugins lib32-libpulse \
+                      lib32-gst-plugins-base lib32-gst-plugins-good \
+                      steam lutris
